9edc7481d1ec0f81067e48a8c5003d14ec7cbee9
Satnam Brar <satnamsbrar@gmail.com>
2017-01-23 11:20:44 -0500

    first commit

---
+++ SB 012317 Inheritance Practice/BallSport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB_012317_Inheritance_Practice
+{
+    class BallSport
+    {
+    }
+}
